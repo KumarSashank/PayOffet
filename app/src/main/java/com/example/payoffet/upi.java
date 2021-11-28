5b4ac8b320834692c15fa30f92b65f3a6bc3d210
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2021-11-29 04:20:08 +0530

    Added upi page and success page

--- app/src/main/java/com/example/payoffet/upi.java
+++ app/src/main/java/com/example/payoffet/upi.java
@@ -8,27 +8,26 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class send_money extends AppCompatActivity {
-    ImageView msend;
+public class upi extends AppCompatActivity {
+    ImageView mdone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_send_money);
-        msend=findViewById(R.id.send);
-        msend.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_upi);
+        mdone=findViewById(R.id.done);
+        mdone.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "send", Toast.LENGTH_SHORT).show();
-                upi();
+                Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_SHORT).show();
+                sent();
             }
         });
-
     }
 
-    public void upi()
+    public void sent()
     {
-        Intent intent = new Intent(this,upi.class);
+        Intent intent = new Intent(this,done.class);
         startActivity(intent);
     }
 }
