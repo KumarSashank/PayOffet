094508ee9bf4db12c368c6b03d20c4a18770994f
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2021-11-29 02:57:24 +0530

    Added sent page

--- app/src/main/java/com/example/payoffet/Payment.java
+++ app/src/main/java/com/example/payoffet/Payment.java
@@ -9,12 +9,14 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Payment extends AppCompatActivity {
-    ImageView mImageView;
+    ImageView mImageView,onlineview,offlineview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
         mImageView=findViewById(R.id.home_pay);
+        onlineview=findViewById(R.id.online_button);
+        offlineview=findViewById(R.id.offline_button);
         mImageView.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -23,10 +25,33 @@ public class Payment extends AppCompatActivity {
                 home();
             }
         });
+
+        onlineview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Online Mode", Toast.LENGTH_SHORT).show();
+                pay();
+            }
+        });
+
+        offlineview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Offline Mode", Toast.LENGTH_SHORT).show();
+                pay();
+            }
+        });
+
     }
     public void home()
     {
         Intent intent = new Intent(this,Home.class);
+        startActivity(intent);
+    }
+
+    public void pay()
+    {
+        Intent intent = new Intent(this,send_money.class);
         startActivity(intent);
     }
 }
