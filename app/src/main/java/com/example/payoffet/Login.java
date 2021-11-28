b050098006a88598849bfc24282e7d2ba056d7b5
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2021-11-28 21:58:58 +0530

    Added signup page

--- app/src/main/java/com/example/payoffet/Login.java
+++ app/src/main/java/com/example/payoffet/Login.java
@@ -2,13 +2,31 @@ package com.example.payoffet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
+    ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mImageView=findViewById(R.id.get_otp);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "otp sent", Toast.LENGTH_SHORT).show();
+                getotp();
+            }
+        });
+    }
+    public void getotp()
+    {
+        Intent intent = new Intent(this,verification.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/payoffet/verification.java
@@ -0,0 +1,17 @@
+package com.example.payoffet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class verification extends AppCompatActivity {
+    ImageView mImageView;
+    TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+}
