feed5fc0632a6d739ec0d540c6449a0a4371f385
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2021-11-29 01:53:36 +0530

    Added Payment page

--- app/src/main/java/com/example/payoffet/Home.java
+++ app/src/main/java/com/example/payoffet/Home.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+    ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +19,19 @@ public class Home extends AppCompatActivity {
         Intent intent=getIntent();
         String personal;
         personal = intent.getStringExtra("Key");
+        mImageView=findViewById(R.id.payment);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  Toast.makeText(getApplicationContext(), "Payments", Toast.LENGTH_SHORT).show();
+                  pay();
+              }
+          }
+        );
+    }
+    public void pay()
+    {
+        Intent intent = new Intent(this,Payment.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/payoffet/Payment.java
@@ -0,0 +1,32 @@
+package com.example.payoffet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Payment extends AppCompatActivity {
+    ImageView mImageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+        mImageView=findViewById(R.id.home_pay);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Home Page", Toast.LENGTH_SHORT).show();
+                home();
+            }
+        });
+    }
+    public void home()
+    {
+        Intent intent = new Intent(this,Home.class);
+        startActivity(intent);
+    }
+}
